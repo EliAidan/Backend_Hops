e9c5fac5eebc5551dbf068f46005f330b1e18c4b
Eli <210538@utxicotepec.edu.mx>
2024-07-19 13:39:50 -0600

    cambios en la bd

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_dFDPjQErQIyDihF1cwP@baseremota-utxicotepec-f0a8.h.aivencloud.com:24758/defaultdb"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base=declarative_base()
